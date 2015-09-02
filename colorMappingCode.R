40778ed218667727e87f9fb5ffedc58cc270fd39
zellw <czweaver@hotmail.com>
2015-09-02 12:56:54 -0400

    Color Mapping - Interactive
    Tested from RPubs document

---
+++ colorMappingCode.R
@@ -0,0 +1,32 @@
+library(rgdal)
+library(leaflet)
+library(RColorBrewer)
+
+tmp <- tempdir()
+
+url <- "http://personal.tcu.edu/kylewalker/data/mexico.zip"
+
+file <- basename(url)
+
+download.file(url, file)
+
+unzip(file, exdir = tmp)
+
+mexico <- readOGR(dsn = tmp, layer = "mexico", encoding = "UTF-8")
+
+head(mexico@data)
+
+pal <- colorQuantile("YlGn", NULL, n = 5)
+
+state_popup <- paste0("<strong>Estado: </strong>", 
+                      mexico$name, 
+                      "<br><strong>PIB per c?pita, miles de pesos, 2008: </strong>", 
+                      mexico$gdp08)
+
+leaflet(data = mexico) %>%
+     addProviderTiles("CartoDB.Positron") %>%
+     addPolygons(fillColor = ~pal(gdp08), 
+                 fillOpacity = 0.8, 
+                 color = "#BDBDC3", 
+                 weight = 1, 
+                 popup = state_popup)
